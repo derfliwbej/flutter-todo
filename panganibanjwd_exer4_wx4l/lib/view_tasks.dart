440a1775742e8b9af89bede3b1e6ad81e36eb844
Jeb <panganibanjebwilfred@gmail.com>
2022-05-29 22:12:16 +0800

    Refactor: Edited TaskList template

--- panganibanjwd_exer4_wx4l/lib/view_tasks.dart
+++ panganibanjwd_exer4_wx4l/lib/view_tasks.dart
@@ -3,9 +3,8 @@ import 'dbhandler.dart';
 import 'TaskModel.dart';
 
 class TaskList extends StatefulWidget {
-  final DatabaseHandler dbHandler;
 
-  const TaskList({Key? key, required this.dbHandler }) : super(key: key);
+  const TaskList({ Key? key }) : super(key: key);
 
   @override
   State<TaskList> createState() => _TaskListState();
@@ -20,32 +19,7 @@ class _TaskListState extends State<TaskList> {
             appBar: AppBar(
                 title: const Text('To Do List')
             ),
-            body: FutureBuilder(
-                future: widget.dbHandler.retrieveTasks(),
-                builder: (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: snapshot.data?.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                              key: ValueKey<int>(snapshot.data![index].id!),
-                              title: Text(snapshot.data![index].title.toString()),
-                              subtitle: Text(snapshot.data![index].details.toString()),
-                              trailing: IconButton(
-                                  icon: Icon(Icons.delete_outline),
-                                  onPressed: () async {
-                                    await widget.dbHandler.deleteTask(snapshot.data![index].id!);
-                                    setState(() {});
-                                  }
-                              )
-                          );
-                        }
-                    );
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                }
-            )
+            body: const Text('This contains the To Do List')
         )
     );
   }
