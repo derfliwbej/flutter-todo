5ff5857b6322e5d5a24dcec8c1e22eaebaba42d8
Jeb <panganibanjebwilfred@gmail.com>
2022-05-29 22:56:56 +0800

    Feat: Added createTask function

--- panganibanjwd_exer4_wx4l/lib/add_task.dart
+++ panganibanjwd_exer4_wx4l/lib/add_task.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'view_tasks.dart';
 
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<http.Response> createTask(String title) {
+  return http.post(
+    Uri.parse('https://jsonplaceholder.typicode.com/todos'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+}
+
 class AddTask extends StatefulWidget {
   const AddTask({Key? key}) : super(key: key);
 
